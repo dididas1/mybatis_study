550a664eb348a0595a943cf9d84a2fe3b799271b
lsw <dididas@naver.com>
2017-03-09 12:31:36 +0900

    TEST

--- src/main/java/kr/or/digt/mybatis_study/dao/StudentDao.java
+++ src/main/java/kr/or/digt/mybatis_study/dao/StudentDao.java
@@ -7,4 +7,8 @@ import kr.or.digt.mybatis_study.dto.Student;
 public interface StudentDao {
 	int insertStudent(Student student);
 	List<Student> selectStudentByAll();
+	// 과제 테스트까지 완료 branch이용해서
+	Student selectStudentByNo(int studNo);
+/*	int updateStudent(Student student);
+	int deleteStudnet(int stuNo);*/
 }
