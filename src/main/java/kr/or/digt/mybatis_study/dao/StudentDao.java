ab91de936918831fc197ce1e7a95269a02bdb7ef
lsw <dididas@naver.com>
2017-03-09 10:48:22 +0900

    studentdao-> selectstudentByAll 추가

---
+++ src/main/java/kr/or/digt/mybatis_study/dao/StudentDao.java
@@ -0,0 +1,10 @@
+package kr.or.digt.mybatis_study.dao;
+
+import java.util.List;
+
+import kr.or.digt.mybatis_study.dto.Student;
+
+public interface StudentDao {
+//	int insertStudent(Student student);
+	List<Student> selectStudentByAll();
+}
